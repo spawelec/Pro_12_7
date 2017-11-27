8b1f6aef7116decd08fd8e765163028d1551d3f1
Sylwia <sylwia.pawelec@wp.pl>
2017-11-27 22:26:46 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    "X-Client-Id": 2565,
+    "X-Auth-Token": "833ca699090df55de131a7c9a9cdf56c"
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
